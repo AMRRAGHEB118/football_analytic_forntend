3f7a7b058762435d37317ad46620da176b8c721e
AMRRAGHEB118 <amrragheb118@gmail.com>
2024-08-02 21:03:13 +0300

    Create and Add header to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} flex items-center justify-center min-h-[100vh] bg-secondary-1000`}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={`${inter.className} min-h-screen bg-secondary-1000`}>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </>
   );
 }
