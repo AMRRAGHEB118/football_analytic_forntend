97a7c10820870106b72185e2512da03368c89a60
AMRRAGHEB118 <amrragheb118@gmail.com>
2024-08-24 18:13:47 +0300

    Fix background color in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
-        <body className={`${inter.className} min-h-screen bg-secondary-1000`}>
+        <body className={`${inter.className} min-h-screen bg-secondary-1100`}>
           <Header />
           {children}
         </body>
