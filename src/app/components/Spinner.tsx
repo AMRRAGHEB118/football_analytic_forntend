6bcaed14b02b0c2d6800e853a78abe38099fa06d
Makarona10 <ahmedneyssi@gmail.com>
2024-10-25 16:07:41 +0300

    Build -> Loading spinner created

---
+++ src/app/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export function LoadingSpinner() {
+    return (
+        <div className="flex justify-center items-center h-32 w-32 m-auto">
+            <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
+        </div>
+    );
+}
